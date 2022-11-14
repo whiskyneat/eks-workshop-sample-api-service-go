96aa469b2ce31d0968d0e72ba5784f674d00035d
whiskyneat <79270785+whiskyneat@users.noreply.github.com>
2022-11-14 12:37:22 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Easy PZ"}
+		res := &response{Message: "Hello World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
