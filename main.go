d5b20348b1831025179d76fdb3c31f2f7e6d3c62
whiskyneat <79270785+whiskyneat@users.noreply.github.com>
2022-11-14 12:43:04 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Docker Increase Rate Limit :P "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
